a52cfef844d0a6b74bf1de59385927d611065ab3
Jacob Dale <jacobscottdale@gmail.com>
2020-09-22 11:59:42 -0400

    Prepared for deploy

--- server.js
+++ server.js
@@ -1,16 +1,56 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const POKEDEX = require('./pokedex.json');
 
 const app = express();
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-  res.send('Hello, sworld!');
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+app.use(function validateBearerToken(req, res, next) {
+  const bearerToken = req.get('Authorization').split(' ')[1];
+  const apiToken = process.env.API_TOKEN;
+  next();
 });
 
-const PORT = 8000;
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
+  if (req.query.name) {
+    response = response.filter(pokemon => pokemon.name.toLowerCase().includes(req.query.name.toLowerCase()));
+  }
+
+  if (req.query.type) {
+    response = response.filter(pokemon => pokemon.type.toLocaleString().toLowerCase().split(',').includes(req.query.type.toLowerCase()));
+  }
+  res.json(response);
+
+}
+
+app.get('/pokemon', handleGetPokemon);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => { });
